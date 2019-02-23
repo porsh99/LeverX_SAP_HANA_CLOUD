c06af3d07ca2077cd9896a802a448fedf26603b9
porsh99 <kladovlesha@mail.ru>
2019-02-23 18:21:00 +0300

    ...

--- hw5_2/web/user_display/webapp/controller/Display.controller.js
+++ hw5_2/web/user_display/webapp/controller/Display.controller.js
@@ -1,13 +1,21 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"sap/m/Button",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, Button, JSONModel) {
-	"use strict";
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/Fragment",
+	'sap/m/MessageBox'
 
+], function (Controller, MessageToast, JSONModel, Fragment, MessageBox) {
+	"use strict";
+	var isCreateOrEdit = false; // true-> Create car, false--> edit mode
 	var sCar;
 	var oViewModel;
+	this.getCreateOrEdit = function () {
+		return isCreateOrEdit;
+	}
+	this.setCreateOrEdit = function (value) {
+		isCreateOrEdit = value;
+	}
 	this.getCar = function () {
 		return sCar;
 	}
@@ -20,22 +28,21 @@ sap.ui.define([
 	this.setViewModel = function (value) {
 		oViewModel = value;
 	}
-
-	this.checkCarObject = function(oCar) {
-		if (oCar == null)
-		return false;
-		if (oCar.crid == null)
-		return false;
-		if (oCar.brend == null)
-		return false;
-		if (oCar.model == null)
-		return false;
-		if (oCar.generation == null)
-		return false;
-		if (oCar.ts_update == null)
-		return false;
-		if (oCar.ts_create == null)
-		return false;
+	this.checkCarObjectToUpdate = function (oCar) {
+		if (oCar == null || oCar.crid == null || oCar.crid === "" || oCar.brend == null || oCar.brend === "" || oCar.model == null || oCar.model === "" || oCar.generation == null || oCar.generation === "")
+			return false;
+		if (oCar.ts_update != null)
+			return false;
+		if (oCar.ts_create != null)
+			return false;
+		else
+			return true;
+	}
+	this.checkCarObjectToCreate = function (oCar) {
+		if (oCar == null || oCar.brend == null || oCar.brend === "" || oCar.model == null || oCar.model === "" || oCar.generation == null || oCar.generation === "")
+			return false;
+		else
+			return true;
 	}
 	this.getLabelName = function (i18nName) {
 		return getViewModel().getModel("i18n").getResourceBundle().getText(i18nName);
@@ -43,30 +50,16 @@ sap.ui.define([
 	this.showMessage = function (message) {
 		MessageToast.show(message);
 	}
-	this.getObjectValueById = function(id)
-	{
+	this.getObjectValueById = function (id) {
 		return getViewModel().byId(id).getValue();
 	}
 
-	this.getNewButton = function (text, type, icon, funPress) {
-		var button = new Button({
-			text: text,
-			type: type,
-			icon: icon,
-			press: function () {
-				funPress.call();
-			}
-		})
-		return button;
-	}
-
-	this.saveCarChanges = function () {
+	this.applyCarChanges = function () {
 		{
-			//getViewModel().byId("newCarBrend").getValue(),
-			 var carID = getCar().crid;
-			 var carBrend = sap.ui.getCore().byId("newCarBrend").getValue();
-			 var carModel = sap.ui.getCore().byId("newCarModel").getValue();
-			 var carGeneration = sap.ui.getCore().byId("newcarGeneration").getValue();
+			var carID = getCar().crid;
+			var carBrend = getViewModel().byId("newCarBrend").getValue();
+			var carModel = getViewModel().byId("newCarModel").getValue();
+			var carGeneration = getViewModel().byId("newCarGeneration").getValue();
 
 			var oObject = {};
 			oObject = {
@@ -78,193 +71,194 @@ sap.ui.define([
 				"ts_create": null
 			};
 
-			console.log(oObject);
+			if (checkCarObjectToUpdate(oObject)) {} else {
+				showMessage(getLabelName("mesgIncorectDataToUpdate"));
+				return;
+			}
 
 
-
-			showMessage(checkCarObject(oObject));
-
-			var oModel = getViewModel().getModel("cars");
-			//console.log(oModel);
-
-			oModel.update("/Cars('" + carID + "')", oObject, {
+			getViewModel().getModel("cars").update("/Cars('" + carID + "')", oObject, {
 				merge: false,
 				success: function () {
 					jQuery.sap.log.info("Sucsess");
+					showMessage(getLabelName("mesgCompleteUpdate"));
 				},
 				error: function () {
 					jQuery.sap.log.error("Error");
+					showMessage(getLabelName("mesgErrorOfPutOdata"));
 				}
 			});
 
 		}
 	}
 
+	this.createCar = function () {
+		var carBrend = getViewModel().byId("newCarBrend").getValue();
+		var carModel = getViewModel().byId("newCarModel").getValue();
+		var carGeneration = getViewModel().byId("newCarGeneration").getValue();
+
+		var oObject = {};
+		oObject = {
+			"brend": carBrend,
+			"model": carModel,
+			"generation": carGeneration
+		};
+
+		if (checkCarObjectToCreate(oObject)) {} else {
+			showMessage(getLabelName("mesgIncorectDataToUpdate"));
+			return;
+		}
+
+		getViewModel().getModel("cars").create("/Cars", oObject, {
+			merge: false,
+			success: function () {
+				jQuery.sap.log.info("Sucsess");
+				showMessage(getLabelName("mesgCompleteCreateCar"));
+			},
+			error: function () {
+				jQuery.sap.log.error("Error");
+				showMessage(getLabelName("mesgErrorOfPostOdata"));
+			}
+		});
+	}
+
+	this.deleteSelectCar = function () {
+		var id = getCar().crid;
+		var settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://porsh99-dev-service.cfapps.eu10.hana.ondemand.com/xsjs/car/car.xsjs?carid=" + id,
+			"method": "DELETE",
+			"headers": {
+				"content-type": "application/json"
+			},
+			"processData": false
+		};
+		$.ajax(settings).done(function () {
+			getViewModel().getModel("cars").refresh(true);
+		});
+	}
+
+	this.showCarDialog = function (controller) {
+		var oView = getViewModel();
+		if (!oView.byId("carDialog")) {
+			Fragment.load({
+				id: oView.getId(),
+				type: "XML",
+				name: "user_display.view.CarDialogFields",
+				controller: controller
+			}).then(function (oDialog) {
+				oView.addDependent(oDialog);
+				oDialog.open();
+			});
+		} else {
+			getViewModel().byId("newCarBrend").setValue("");
+			getViewModel().byId("newCarModel").setValue("");
+			getViewModel().byId("newCarGeneration").setValue("");
+			oView.byId("carDialog").open();
+		}
+	}
+
 	return Controller.extend("user_display.controller.Detail", {
 		onInit: function () {
-
-			var mod = this.getView().getModel("init_data");
-			//MessageToast.show(mod);
-			//var oModel = new JSONModel("user_display/model/init_data.json");
-			console.log("_____________");
-			//console.log(oModel);
-			console.log(mod);
-
 			setViewModel(this.getView());
-			if (!this._dialog) {
-				this._dialog = sap.ui.xmlfragment("user_display.view.BusyDialog", this);
-				this.getView().addDependent(this._dialog);
-			}
 
-			jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._dialog);
-			this._dialog.open();
+			// var mod = this.getView().getModel("init_data");
+			// MessageToast.show(mod);
+			// console.log(mod);
+			//var oModel = new JSONModel("user_display/model/init_data.json");
+			//console.log("_____________");
+			//console.log(oModel);
+			//console.log(mod);
 
-			_timeout = jQuery.sap.delayedCall(300, this, function () {
-				this._dialog.close();
-			});
+			// var mod = new sap.ui.model.json.JSONModel();
+			// mod.loadData("model/init_data.json");
+			// //sap.ui.getCore().setModel(mod, "data");
+			// this.getView().setModel(mod, "data");
+
+			//var dataModel = this.getOwnerComponent().getModel("init_data");
+			//this.getView().setModel(dataModel, "init_data");
+
+			// if (!this._dialog) {
+			// 	this._dialog = sap.ui.xmlfragment("user_display.view.BusyDialog", this);
+			// 	this.getView().addDependent(this._dialog);
+			// }
+
+			// jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._dialog);
+			// this._dialog.open();
+
+			// _timeout = jQuery.sap.delayedCall(300, this, function () {
+			// 	this._dialog.close();
+			// });
 		},
-		createUser: function () {
-			var Name = sap.ui.getCore().byId(this.getView().sId + "--input_name").getValue();
-			console.log(Name);
-			var settings = {
-				"async": true,
-				"crossDomain": true,
-				"url": "https://porsh99-dev-service.cfapps.eu10.hana.ondemand.com/xsodata/hw3.xsodata/Users",
-				"method": "POST",
-				"headers": {
-					"content-type": "application/json"
-				},
-				"processData": false,
-				"data": "{\"name\": \"" + Name + "\"}"
-			};
-
-			$.ajax(settings).done(function (response) {
-				console.log(response);
-			});
-
-			window.location.reload();
-		},
-		updateUser: function () {
-			var Name = sap.ui.getCore().byId(this.getView().sId + "--input_name").getValue();
-			var UserID = sap.ui.getCore().byId(this.getView().sId + "--input_id").getValue();
-			console.log(Name);
-			var settings = {
-				"async": true,
-				"crossDomain": true,
-				"url": "https://porsh99-dev-service.cfapps.eu10.hana.ondemand.com/xsodata/hw3.xsodata/Users('" + UserID + "')",
-				"method": "PUT",
-				"headers": {
-					"content-type": "application/json"
-				},
-				"processData": false,
-				"data": "{\"name\": \"" + Name + "\",\"ts_update\": null,	\"ts_create\": null}"
-			};
-
-			$.ajax(settings).done(function (response) {
-				console.log(response);
-			});
-
-			window.location.reload();
-		},
-
 		selectTableCarsItem: function () {
-			var oTable = getViewModel().byId("details");
-			setCar(oTable.getSelectedItem().getBindingContext("cars").getObject());
+			showMessage(getLabelName("mesgSelectRow"));
 		},
-		
-		onClickEdit: function (oEvent) {
+		onClickEditCar: function () {
 
+			setCreateOrEdit(false); // set edit mode
+			var oTable = getViewModel().byId("tableCars");
+			try {
+				setCar(oTable.getSelectedItem().getBindingContext("cars").getObject()); // Update last select values of row after Update Model
+			} catch {
+				showMessage(getLabelName("mesgUnselectRow"));
+				return;
+			}
 			var oCar = getCar();
-			if (oCar.crid == null) {
-				showMessage("You should select item at first");
+			if (oCar == null) {
+				showMessage(getLabelName("mesgUnselectRow"));
 				return;
 			}
 
-			var dialog = new sap.m.Dialog({
-				title: getLabelName("headerCarEditDialog"),
-				type: "Message",
-				content: [
-					new sap.ui.layout.VerticalLayout({
-						width: "350px",
-						content: [
-							new sap.ui.layout.HorizontalLayout({
-								content: [
-									new sap.m.Label({
-										width: "100px",
-										design: "Bold",
-										text: getLabelName("labelCarBrend")
-									}).addStyleClass("popup_label"),
-									new sap.m.Input("newCarBrend", {
-										width: "230px",
-										value: oCar.brend
-									})
-								]
-							}),
-							new sap.ui.layout.HorizontalLayout({
-								content: [
-									new sap.m.Label({
-										width: "100px",
-										design: "Bold",
-										text: getLabelName("labelCarModel")
-									}).addStyleClass("popup_label"),
-									new sap.m.Input("newCarModel", {
-										value: oCar.model,
-										width: "230px"
-									})
-								]
-							}),
-							new sap.ui.layout.HorizontalLayout({
-								content: [
-									new sap.m.Label({
-										width: "100px",
-										design: "Bold",
-										text: getLabelName("labelCarGeneration")
-									}).addStyleClass("popup_label"),
-									new sap.m.Input("newcarGeneration", {
-										value: oCar.generation,
-										width: "230px"
-									})
-								]
-							})
-						]
-					})
-				],
-				beginButton: getNewButton(
-					"",
-					"Accept",
-					"sap-icon://save",
-					function () {
-						setCar(oCar);
-						saveCarChanges();
-						dialog.close();
-					}
-				),
-				endButton: getNewButton(
-					"",
-					"Reject",
-					"sap-icon://decline",
-					function () {
-						dialog.close();
-					}
-				),
-				afterClose: function () {
-					dialog.destroy();
-				}
-			});
-			dialog.open();
-			setViewModel(this.getView());
+			showCarDialog(this);
+
+			this.byId("newCarBrend").setValue(oCar.brend);
+			this.byId("newCarModel").setValue(oCar.model);
+			this.byId("newCarGeneration").setValue(oCar.generation);
 		},
-		_getDialog : function () {
-			if (!this._oDialog) {
-			   this._oDialog = sap.ui.xmlfragment("user_display.view.CarDialogFields");
-			   
-			   this.getView().addDependent(this._oDialog);
+		onClickCreateCar: function () {
+			setCreateOrEdit(true); // set create mode
+			showCarDialog(this);
+		},
+		onCloseDialog: function () {
+			this.byId("carDialog").close();
+		},
+		saveCarChaneges: function () {
+			if (!getCreateOrEdit()) {
+				applyCarChanges();
+			} else {
+				createCar();
 			}
-			return this._oDialog;
-		 },
-		 onOpenDialog : function () {
-			this._getDialog().open();
-		 }
+			this.byId("carDialog").close();
+		},
+		onDeleteCar: function () {
+
+			setCreateOrEdit(false); // set edit mode
+			var oTable = getViewModel().byId("tableCars");
+			try {
+				setCar(oTable.getSelectedItem().getBindingContext("cars").getObject()); // Update last select values of row after Update Model
+			} catch {
+				showMessage(getLabelName("mesgUnselectRow"));
+				return;
+			}
+			var oCar = getCar();
+			if (oCar == null) {
+				showMessage(getLabelName("mesgUnselectRow"));
+				return;
+			}
+
+			var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
+			MessageBox.confirm(
+				getLabelName("confirmTextToDeleteCar"), {
+					styleClass: bCompact ? "sapUiSizeCompact" : "",
+					onClose: function (oAction) {
+						if (oAction === "OK") {
+							deleteSelectCar();
+						}
+					}
+				}
+			);
+		}
 
 	});
 });
